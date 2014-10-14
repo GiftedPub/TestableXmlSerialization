e71b402133e8eeda922c435541b31a35578e93af
Matt Sell <matt.sell@improvingenterprises.com>
2014-10-14 13:51:25 -0500

    add xml deserialization

--- TestableXmlSerialization/XmlSerializerWrapper.cs
+++ TestableXmlSerialization/XmlSerializerWrapper.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.IO.Abstractions;
 using System.Xml.Serialization;
 
 namespace TestableXmlSerialization
@@ -6,16 +7,32 @@ namespace TestableXmlSerialization
     public interface IXmlSerializer
     {
         void Serialize(Stream stream, object o);
+        T Deserialize<T>(string path);
     }
 
     public class XmlSerializerWrapper : IXmlSerializer
     {
         XmlSerializer _xmlSerializer;
+        readonly IFileSystem _fileSystem;
+
+        public XmlSerializerWrapper(IFileSystem fileSystem)
+        {
+            _fileSystem = fileSystem;
+        }
 
         public void Serialize(Stream stream, object o)
         {
             _xmlSerializer = new XmlSerializer(o.GetType());
             _xmlSerializer.Serialize(stream, o);
+        }
+
+        public T Deserialize<T>(string path)
+        {
+            using (var fs = _fileSystem.File.OpenRead(path))
+            {
+                var ser = new XmlSerializer(typeof(T));
+                return (T)ser.Deserialize(fs);
+            }
         }
     }
 }
