45bc657e81d175ae57994dfd1b1a2a1e583cb579
Kris McGinnes <kmcginnes@me.com>
2014-11-19 14:54:35 -0600

    Added overloads taking TextReader and stream

--- TestableXmlSerialization/XmlSerializerWrapper.cs
+++ TestableXmlSerialization/XmlSerializerWrapper.cs
@@ -7,12 +7,14 @@ namespace TestableXmlSerialization
     public interface IXmlSerializer
     {
         void Serialize(Stream stream, object o);
+        void Serialize(TextWriter textWriter, object o);
         T Deserialize<T>(string path);
+        T Deserialize<T>(TextReader stringReader);
+        T Deserialize<T>(Stream stream);
     }
 
     public class XmlSerializerWrapper : IXmlSerializer
     {
-        XmlSerializer _xmlSerializer;
         readonly IFileSystem _fileSystem;
 
         public XmlSerializerWrapper(IFileSystem fileSystem)
@@ -22,8 +24,14 @@ namespace TestableXmlSerialization
 
         public void Serialize(Stream stream, object o)
         {
-            _xmlSerializer = new XmlSerializer(o.GetType());
-            _xmlSerializer.Serialize(stream, o);
+            var ser = new XmlSerializer(o.GetType());
+            ser.Serialize(stream, o);
+        }
+
+        public void Serialize(TextWriter textWriter, object o)
+        {
+            var ser = new XmlSerializer(o.GetType());
+            ser.Serialize(textWriter, o);
         }
 
         public T Deserialize<T>(string path)
@@ -33,6 +41,18 @@ namespace TestableXmlSerialization
                 var ser = new XmlSerializer(typeof(T));
                 return (T)ser.Deserialize(fs);
             }
+        }
+
+        public T Deserialize<T>(TextReader stringReader)
+        {
+            var ser = new XmlSerializer(typeof(T));
+            return (T) ser.Deserialize(stringReader);
+        }
+
+        public T Deserialize<T>(Stream stream)
+        {
+            var ser = new XmlSerializer(typeof(T));
+            return (T) ser.Deserialize(stream);
         }
     }
 }
